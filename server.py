9dec79d1db4c21c501d877c1abc3efa2e1457c80
x13machine <brandanw2@gmail.com>
2016-05-16 19:01:10 -0500

    Create server.py

---
+++ server.py
@@ -0,0 +1,70 @@
+from datetime import *
+import socket
+import sys
+
+#settings
+HOST = ''
+PORT = 4444
+KEY="qgpq6XjqH8m5P7AS" # needs to be random so people don't do hacking thing
+TIMEOUT = 30 #seconds
+
+servers = {}
+# Datagram (udp) socket
+try:
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	print 'Socket created'
+except socket.error, msg :
+	print 'Failed to create socket. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+	sys.exit()
+ 
+ 
+# Bind socket to local host and port
+try:
+	s.bind((HOST, PORT))
+except socket.error , msg:
+	print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+	sys.exit()
+	 
+print 'Socket bind complete'
+
+def update():
+	servs=[]
+	for i in servers:
+		if (datetime.now()-servers[i]).total_seconds() > TIMEOUT:
+			servs+=[i]
+	for i in servs:
+		servers.pop(i,None)
+
+while 1:
+	# receive data from client (data, addr)
+	d = s.recvfrom(1024)
+	data = d[0]
+	addr = d[1]
+	
+	parts=data.split(' ')
+	
+	if len(parts)<2:
+		continue
+	
+	if parts[0] != KEY:
+		continue
+	cmd = parts[1].lower()
+
+	info = " ".join(parts[2:])
+
+	
+	update()
+	
+	print('cmd',cmd)
+	if cmd == 'ping':
+		servers[info] = datetime.now()
+		s.sendto(KEY+' pong' , addr)
+	elif cmd == 'ls':
+		servs = ''
+		for i in servers:
+			servs +=' '+i
+		s.sendto(KEY+' ls' + servs , addr)
+		
+	print 'Message[' + addr[0] + ':' + str(addr[1]) + '] - ' + data.strip()
+	 
+s.close()
